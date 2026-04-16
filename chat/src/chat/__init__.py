d9fbc356d2c4144984d477959a2bd68535175ec7
unknown <spencerphoton@gmail.com>
2026-04-16 17:15:20 -0400

    created some backend handling

--- chat/src/chat/__init__.py
+++ chat/src/chat/__init__.py
@@ -1,6 +1,11 @@
 from quart import Quart
+from quart import render_template
 
 app = Quart(__name__)
 
 def run() -> None:
     app.run()
+
+@app.get("/")
+async def index():
+    return await render_template("index.html")
