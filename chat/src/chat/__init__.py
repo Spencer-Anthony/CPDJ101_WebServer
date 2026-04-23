1dc092b8abeed48ebf638bcef6bbaf8b4d178e62
unknown <spencerphoton@gmail.com>
2026-04-23 15:22:37 -0400

    added some functionality

--- chat/src/chat/__init__.py
+++ chat/src/chat/__init__.py
@@ -1,5 +1,8 @@
 from quart import Quart
 from quart import render_template
+import asyncio
+from quart import websocket
+from chat.broker import Broker
 
 app = Quart(__name__)
 
@@ -9,3 +12,20 @@ def run() -> None:
 @app.get("/")
 async def index():
     return await render_template("index.html")
+
+broker = Broker()
+
+async def _receive() -> None:
+    while True:
+        message = await websocket.receive()
+        await broker.publish(message)
+
+@app.websocket("/ws")
+async def ws() -> None:
+    try:
+        task = asyncio.ensure_future(_receive())
+        async for message in broker.subscribe():
+            await websocket.send(message)
+    finally:
+        task.cancel()
+        await task
